66e10f2ecae0f01e6e2dc0255fd21452ba2d3114
James Lenahan <james.k.lenahan@gmail.com>
2020-03-06 16:39:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const append = ["Milo", "Otis", "Garfield"];
 append.push(Odie);
+append;
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"]
